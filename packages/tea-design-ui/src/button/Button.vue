25794d5efebb2a94e329c849b2b11c8607d300f9
corner <sptdfdzl@163.com>
2022-06-03 13:52:11 +0800

    feat(初始化仓库):初始化monorepo配置，使用vite创建tea-design-ui

---
+++ packages/tea-design-ui/src/button/Button.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <button>按钮</button>
+</template>
+
+<style lang="less">
+@import url('../styles/normalize.css');
+
+button {
+  color: red;
+}
+</style>
---
+++ packages/tea-design-ui/src/button/example/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TeaButton from '../Button.vue';
+</script>
+
+<template>
+  <div>
+    <h1>1</h1>
+    <TeaButton></TeaButton>
+  </div>
+</template>
